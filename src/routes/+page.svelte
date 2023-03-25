3ab9a98670d8b639710f2e644ab456d4cb3b552e
WAYOFFprod <raphael@wayoff.ch>
2023-03-25 18:56:51 +0100

    extra large for test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,3 @@
 <div class="container min-h-screen flex justify-center items-center">
-  <h1 class="text-3xlfont-bold text-center">WAAM</h1>
+  <h1 class="text-6xl font-bold text-center">WAAM</h1>
 </div>
